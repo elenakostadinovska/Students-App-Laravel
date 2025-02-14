ec2c13d78f04b58da90e0a051cc5929b7dfa5cd5
elenakostadinovska <93331149+elenakostadinovska@users.noreply.github.com>
2025-02-14 18:20:41 +0100

    Changed route of dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\UniversityController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UniversityApiController;
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 // Resource route for StudentController
 Route::resource('students', StudentController::class);
