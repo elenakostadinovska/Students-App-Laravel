ecf2aa35303eadeeb28396110ecd26f4f77bea53
elenakostadinovska <93331149+elenakostadinovska@users.noreply.github.com>
2025-02-14 17:43:11 +0100

    New API commit

--- resources/views/api/index.blade.php
+++ resources/views/api/index.blade.php
@@ -3,31 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Universities in United States</title>
-
+    <title>Population Data in United States</title>
 </head>
 <body>
     @include('layouts.header')
-    <h1>Universities in United States</h1>
+    <h1>Population Data in United States</h1>
 
-    <!-- University Data Table -->
-    <table id="universityTable" class="display" style="width:100%">
+    <!-- Debugging: Print API response -->
+    {{-- <pre>{{ print_r($data, true) }}</pre> --}}
+
+    <!-- Population Data Table -->
+    <table id="populationTable" class="display" style="width:100%">
         <thead>
             <tr>
                 <th>#</th>
-                <th>University Name</th>
-                <th>Country</th>
-                <th>Website</th>
+                <th>Year</th>
+                <th>Nation</th>
+                <th>Population</th>
             </tr>
         </thead>
         <tbody>
             @if (!empty($data) && is_array($data))
-                @foreach ($data as $index => $university)
+                @foreach ($data as $index => $record)
                     <tr>
                         <td>{{ $index + 1 }}</td>
-                        <td>{{ $university['name'] }}</td>
-                        <td>{{ $university['country'] }}</td>
-                        <td><a href="{{ $university['web_pages'][0] }}" target="_blank">Visit Website</a></td>
+                        <td>{{ $record['Year'] ?? 'N/A' }}</td>
+                        <td>{{ $record['Nation'] ?? 'N/A' }}</td>
+                        <td>{{ isset($record['Population']) ? number_format((int) $record['Population']) : 'N/A' }}</td>
                     </tr>
                 @endforeach
             @else
@@ -37,17 +39,17 @@
             @endif
         </tbody>
     </table>
-    @include('layouts.footer')
     
-    <!-- You can include DataTables or other styling libraries here -->
+    @include('layouts.footer')
+
+    <!-- Include DataTables -->
     <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
     <script>
         $(document).ready(function () {
-            // Initialize DataTables for the table with ID universityTable
-            $('#universityTable').DataTable();
+            $('#populationTable').DataTable();
         });
     </script>
 
--- resources/views/students/layout.blade.php
+++ resources/views/students/layout.blade.php
@@ -10,7 +10,6 @@
 <div class="container">
     @yield('content')
 
-   
 </div>
       
 </body>
